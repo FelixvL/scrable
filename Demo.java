b94961c330ca08d649b42767e91163c2a50e5629
FelixvL <digitaleservice@gmail.com>
2019-02-21 13:36:54 +0100

    derde commit

--- Demo.java
+++ Demo.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 class Demo{
 	public static void main(String[] args) {	
 		System.out.println("Scrable");
 		Speler speler1 = new Speler();
 		System.out.println( speler1.plank );
+		System.out.println( speler1.plank.steentje.letter );
 	}
 }
 class Speler{
@@ -10,5 +14,31 @@ class Speler{
 	Plankje plank = new Plankje();
 }
 class Plankje{
-	
+	Steen steentje = Stenenzakje.stenen.get(0);
 }
+class Steen{
+	char letter;
+	Steen(char eenLetter){
+		letter = eenLetter;
+	}
+}
+class Stenenzakje{
+	static ArrayList<Steen> stenen = new ArrayList();
+	static Random husselaar = new Random();
+	static{
+		char getal = (char)(husselaar.nextInt(26) + 65);
+		Steen steentje = new Steen(getal);
+		stenen.add(steentje);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
