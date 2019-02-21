cb9cba2cd2c543d7c584c5a4c7ecd3ab91a19b98
FelixvL <digitaleservice@gmail.com>
2019-02-21 13:11:45 +0100

    tweede commit

--- Demo.java
+++ Demo.java
@@ -1,5 +1,14 @@
 class Demo{
 	public static void main(String[] args) {	
 		System.out.println("Scrable");
+		Speler speler1 = new Speler();
+		System.out.println( speler1.plank );
 	}
+}
+class Speler{
+	String naam;
+	Plankje plank = new Plankje();
+}
+class Plankje{
+	
 }
